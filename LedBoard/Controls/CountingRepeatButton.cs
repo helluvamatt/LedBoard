159aa8d89c1ab893b723d27ff589fc211f35aeeb
Matt Schneeberger <helluvamatt@gmail.com>
2020-07-09 21:45:52 -0500

    Improve configuration property editors

---
+++ LedBoard/Controls/CountingRepeatButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace LedBoard.Controls
+{
+	public class CountingRepeatButton : RepeatButton
+	{
+		private int _RepeatCount;
+
+		public CountingRepeatButton() : base()
+		{
+			CommandParameter = 0;
+		}
+
+		protected override void OnClick()
+		{
+			base.OnClick();
+			CommandParameter = ++_RepeatCount;
+		}
+
+		protected override void OnIsPressedChanged(DependencyPropertyChangedEventArgs e)
+		{
+			CommandParameter = _RepeatCount = 0;
+			base.OnIsPressedChanged(e);
+		}
+	}
+}
--- LedBoard/ViewModels/Config/TimeSpanAdvancedPropertyViewModel.cs
+++ LedBoard/ViewModels/Config/TimeSpanAdvancedPropertyViewModel.cs
@@ -16,12 +16,17 @@ namespace LedBoard.ViewModels.Config
 			SetDuration(initialValue);
 			ShowDefault = showDefault;
 			SetDefaultCommand = new DelegateCommand(() => DefaultRequested?.Invoke(this, EventArgs.Empty));
+			IncreaseCommand = new DelegateCommand<int>(OnIncrease, _ => TsValue < TimeSpan.FromHours(12));
+			DecreaseCommand = new DelegateCommand<int>(OnDecrease, _ => TsValue > TimeSpan.Zero);
 		}
 
-		public override object Value => new TimeSpan(0, Hours, Minutes, Seconds, Milliseconds);
+		public override object Value => TsValue;
 
 		public IEnumerable<int> Values12 => Enumerable.Range(0, 13);
 		public IEnumerable<int> Values60 => Enumerable.Range(0, 60);
+
+		public ICommand IncreaseCommand { get; }
+		public ICommand DecreaseCommand { get; }
 
 		#region Default value support
 
@@ -71,12 +76,17 @@ namespace LedBoard.ViewModels.Config
 
 		#region Milliseconds
 
-		public static readonly DependencyProperty MillisecondsProperty = DependencyProperty.Register(nameof(Milliseconds), typeof(int), typeof(TimeSpanAdvancedPropertyViewModel), new PropertyMetadata(0, OnTimeSpanValueChanged));
+		public static readonly DependencyProperty MillisecondsProperty = DependencyProperty.Register(nameof(Milliseconds), typeof(int), typeof(TimeSpanAdvancedPropertyViewModel), new PropertyMetadata(0, OnTimeSpanValueChanged), ValidateMilliseconds);
 
 		public int Milliseconds
 		{
 			get => (int)GetValue(MillisecondsProperty);
 			set => SetValue(MillisecondsProperty, value);
+		}
+
+		private static bool ValidateMilliseconds(object value)
+		{
+			return value is int val && val >= 0 && val < 1000;
 		}
 
 		#endregion
@@ -87,7 +97,7 @@ namespace LedBoard.ViewModels.Config
 			if (!vm._Initializing) vm.OnValueChanged();
 		}
 
-		public void SetDuration(TimeSpan value)
+		public void SetDuration(TimeSpan value, bool fireUpdate = false)
 		{
 			_Initializing = true;
 			Hours = value.Hours;
@@ -95,6 +105,34 @@ namespace LedBoard.ViewModels.Config
 			Seconds = value.Seconds;
 			Milliseconds = value.Milliseconds;
 			_Initializing = false;
+			CommandManager.InvalidateRequerySuggested();
+			if (fireUpdate) OnValueChanged();
 		}
+
+		private void OnIncrease(int repeatCount)
+		{
+			TimeSpan delta;
+			if (repeatCount > 1000) delta = TimeSpan.FromSeconds(1);
+			else if (repeatCount > 100) delta = TimeSpan.FromMilliseconds(100);
+			else if (repeatCount > 10) delta = TimeSpan.FromMilliseconds(10);
+			else delta = TimeSpan.FromMilliseconds(1);
+
+			var ts = TsValue + delta;
+			if (ts <= TimeSpan.FromHours(12)) SetDuration(ts, true);
+		}
+
+		private void OnDecrease(int repeatCount)
+		{
+			TimeSpan delta;
+			if (repeatCount > 1000) delta = TimeSpan.FromSeconds(1);
+			else if (repeatCount > 100) delta = TimeSpan.FromMilliseconds(100);
+			else if (repeatCount > 10) delta = TimeSpan.FromMilliseconds(10);
+			else delta = TimeSpan.FromMilliseconds(1);
+
+			var ts = TsValue - delta;
+			if (ts >= TimeSpan.Zero) SetDuration(ts, true);
+		}
+
+		private TimeSpan TsValue => new TimeSpan(0, Hours, Minutes, Seconds, Milliseconds);
 	}
 }
